5cb1ccee28b0fff16e8e1dad5b063e76dbc173be
K-yokosu <kkk_ns_ili-1125@ezweb.ne.jp>
2020-07-20 08:51:15 +0000

    all-typeScript.ts add

--- src/all-typeScript.ts
+++ src/all-typeScript.ts
@@ -22,17 +22,135 @@ function returnNothing(): void{
 }
 
 //never 呼び元にもどってこない関数（例外発生に使用）
-function error(message: string): never{
-  throw new Error(message);
-}
+// function error(message: string): never{
+//   throw new Error(message);
+// }
 
-try{
-  let result = error("test");
-  console.log(result);
-} catch(error){
-  console.log(error);
-}
+// try{
+//   let result = error("test");
+//   console.log(result);
+// } catch(error){
+//   console.log(error);
+// }
 
 //null と undefined
 let abc: null = null;
 let def: undefined = undefined;
+
+//object
+let obj: object = {ob: 'kou'};
+let obje: {oB: string} = {oB: 'kou'};
+
+//alias
+type Mojiretu = string;
+const title: Mojiretu = 'title';
+
+type Profile = {
+  sei: string;
+  sai: number;
+}
+const myProfile: Profile = {
+  sei: 'yokota',
+  sai: 24
+}
+
+//interface
+interface objInterface{
+  name: string;
+  age: number;
+}
+let objj: objInterface = {
+  name: 'kou',
+  age: 24
+}
+
+//unknown
+const kansu = (): number => 43;
+let numberUnknown: unknown = kansu();
+if(typeof numberUnknown === 'number'){
+  console.log('unknown = number');
+}
+
+//intersection
+type Pitcher = {
+  speed: number;
+}
+type Batter = {
+  swingSpeed: number;
+}
+type YobandeAce = Pitcher & Batter;
+const kaibutu: YobandeAce = {
+  speed: 150,
+  swingSpeed: 140,
+}
+console.log({kaibutu});
+
+//union
+let value: string | number;
+value = 1;
+value = 'kou';
+
+//Literal
+let dayofTheWeek: '月' = '月';
+let month: 1|2|3 = 1;
+let True: true = true;
+
+//enum
+enum Months{
+  January = 1,
+  Febrary,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  Nobember,
+  December,
+}
+console.log(Months.April);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+}
+
+//function
+//最後のnumber型は戻り地の型を指定する
+function bmi(height: number, weight: number): number{
+  return weight / (height * height);
+}
+console.log(bmi(1.72, 66));
+
+let ave: (sum: number, wal: number) => number = (
+  sum: number,
+  wal: number
+): number => {
+  return sum / wal;
+}
+
+console.log(ave(100, 10));
+
+//optinalパワメータ　あってもなくてもOKな引数のこと
+//argumentsに?をつける
+let bmi2: (height: number, weight: number, printable?: boolean) => number = (
+  height: number, 
+  weight: number, 
+  printable?: boolean
+): number => {
+  return weight / (height * height);
+}
+console.log(bmi2(1.73, 66));
+
+//default paramater
+let defaultParamater: (arg1: number, arg2?: number) => number = (
+  arg1: number,
+  arg2: number = 10
+): number => {
+  return arg1 * arg2;
+}
+console.log(defaultParamater(2));
+
